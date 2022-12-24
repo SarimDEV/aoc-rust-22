b4b7e9224cd43741faf3ca59af312af82a5f8231
Muhammad Sarim <m2sarim@uwaterloo.ca>
2022-12-23 21:00:14 -0500

    less memory

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -1,48 +1,41 @@
 use std::fs;
 use std::collections::HashSet;
-use std::collections::HashMap;
 
 fn main() {
     let str = read_input("input.txt");
     let mut set: HashSet<char> = HashSet::new();
-    let mut map: HashMap<char, u32> = HashMap::new();
     let mut p1: usize = 0;
+    let mut l = 0;
 
-    for (i, c) in str.chars().enumerate() {
-        if set.len() == 4 {
-            p1 = i;
-            break;
+    for (r, c) in str.chars().enumerate() {
+        while set.contains(&c) {
+            let char_to_remove = str.as_bytes()[l] as char;
+            set.remove(&char_to_remove);
+            l += 1;
         }
         set.insert(c);
-        map.insert(c, map.get(&c).unwrap_or(&0)+1);
-        if i < 4 {
-            continue;
-        }
-        let char_to_remove = str.as_bytes()[i-4] as char;
-        map.insert(char_to_remove, map.get(&char_to_remove).unwrap()-1);
-        if map.get(&char_to_remove).unwrap() == &0 {
-            set.remove(&char_to_remove);
+
+        if r - l + 1 >= 4 {
+            p1 = r + 1;
+            break;
         }
     }
 
     let mut set1: HashSet<char> = HashSet::new();
-    let mut map1: HashMap<char, u32> = HashMap::new();
     let mut p2: usize = 0;
+    let mut l = 0;
 
-    for (i, c) in str.chars().enumerate() {
-        if set1.len() == 14 {
-            p2 = i;
-            break;
+    for (r, c) in str.chars().enumerate() {
+        while set1.contains(&c) {
+            let char_to_remove = str.as_bytes()[l] as char;
+            set1.remove(&char_to_remove);
+            l += 1;
         }
         set1.insert(c);
-        map1.insert(c, map1.get(&c).unwrap_or(&0)+1);
-        if i < 14 {
-            continue;
-        }
-        let char_to_remove = str.as_bytes()[i-14] as char;
-        map1.insert(char_to_remove, map1.get(&char_to_remove).unwrap()-1);
-        if map1.get(&char_to_remove).unwrap() == &0 {
-            set1.remove(&char_to_remove);
+
+        if r - l + 1 >= 14 {
+            p2 = r + 1;
+            break;
         }
     }
 
